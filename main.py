eca5580b2ec8eb2840ad5701e54592f2c25e49be
henokjackson <henokjackson@gmail.com>
2022-12-19 10:15:24 +0530

    integrated miner and text preprocessor

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-#hello
+from tweet_miner.miner import TweetMiner
+from tweet_preprocessing import TweetPreprocess
+
+if __name__=='__main__':
+	limit=int(input("Numder of Tweets : "))
+	tweetlist=TweetMiner(limit)
+	TweetPreprocess(tweetlist)
