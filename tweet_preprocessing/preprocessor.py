f8759e7a25e9656998c8c33b819a55e31f538cac
henokjackson <henokjackson@gmail.com>
2022-12-19 16:04:40 +0530

    Updated Miner and Text Preprocessor

--- tweet_preprocessing/preprocessor.py
+++ tweet_preprocessing/preprocessor.py
@@ -9,14 +9,11 @@ def Tweet_Preprocess(tweetlist):
     #tweets mined which is comes as a list of strings tweetlist
     #limit is the number of tweets
 
-    dict={'tweet':tweetlist}
-
-    #convert list of strings to csv file
-    tf=pd.DataFrame(dict)
-    df.to_csv('tweet.csv')
+    clean_txt=[]
 
     #convert to lower case
-    tf['clean_txt']=tf['tweet'].str.lower()
+    for tweet in tweetlist:
+    	clean_txt.append(tweet.lower)
 
     #remove punctuations
     def remove_punctuations(text):
@@ -30,7 +27,7 @@ def Tweet_Preprocess(tweetlist):
     def remove_stopwords(text):
         return ' '.join([word for word in text.spilt() if word not in STOPWORDS])
 
-    tf['clean_txt']=tf['clean_txt'].apply(lambda x:remove_stopwords(x))
+    clean_txt=clean_txt.apply(lambda x:remove_stopwords(x))
 
     #can remove frequent words and rare words if needed
     #remove special characters and punctuations
@@ -39,26 +36,26 @@ def Tweet_Preprocess(tweetlist):
         text=re.sub('\s+',' ',text)
         return text
 
-    tf['clean_txt']=tf['clean_txt'].apply(lambda x:remove_spl_chars(x))
+    clean_txt=clean_txt.apply(lambda x:remove_spl_chars(x))
 
     #stemming
     ps=PorterStemmer()
     def stem_words(text):
         return " ".join([ps.stem(word) for word in text.split()])
 
-    tf['stemmed_txt']=tf['clean_txt'].apply(lambda x:stem_words(x))
+    clean_txt=clean_txt.apply(lambda x:stem_words(x))
 
     #remove URLs
     def remove_url(text):
         return re.sub(r'https?://\S+|www\.\S+', text)
 
-    tf['clean_txt']=tf['clean_txt'].apply(lambda x:remove_url(x))
+    clean_txt=clean_txt.apply(lambda x:remove_url(x))
 
     #remove html tags
     def remove_html_tags(text):
         returnre.sub(r'<.*?>','',text)
 
-    tf['clean_txt']=tf['clean_txt'].apply(lambda x:remove_html_tags(x))
+    clean_txt=clean_txt.apply(lambda x:remove_html_tags(x))
 
     #can perform spelling correction
     # remove twitter handles
@@ -68,6 +65,8 @@ def Tweet_Preprocess(tweetlist):
             input_txt=re.sub(word, "",input_txt)
         return input_txt
 
-    tf['clean_txt']=np.vectorsize(remove_pattern)(tf['clean_txt'],"@[\w]*")
+    clean_txt=np.vectorize(remove_pattern)(clean_txt,"@[\w]*")
+
+    tweettxt=open("cleaned.txt")
 
     #can do tokenization
