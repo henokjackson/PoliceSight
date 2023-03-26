Fixed Bug In Normalization Function

fire_detection/color_detection/Color_Detection_YCbCr.py
@@ -14,32 +14,34 @@ def Localize_Fire(frame):
     #frame_rgb=cv.cvtColor(frame,cv.COLOR_BGR2RGB)
     frame_ycbcr=cv.cvtColor(frame,cv.COLOR_BGR2YCR_CB)
 
-    mask=np.zeros_like(frame)
+    mask=np.zeros_like(frame_ycbcr)
 
+    #'''
     #Params for Rule set - 0
-    Y_Cb_min=np.min(frame[...][...][0]-frame[...][...][2])
-    Y_Cb_max=np.max(frame[...][...][0]-frame[...][...][2])
-    Cr_Cb_min=np.min(frame[...][...][1]-frame[...][...][2])
-    Cr_Cb_max=np.max(frame[...][...][1]-frame[...][...][2])
+    Y_Cb_min=np.min(frame_ycbcr[...][...][0]-frame_ycbcr[...][...][2])
+    Y_Cb_max=np.max(frame_ycbcr[...][...][0]-frame_ycbcr[...][...][2])
+    Cr_Cb_min=np.min(frame_ycbcr[...][...][1]-frame_ycbcr[...][...][2])
+    Cr_Cb_max=np.max(frame_ycbcr[...][...][1]-frame_ycbcr[...][...][2])
 
-    '''
-    Y_min=np.min(frame[...][...][0])
-    Y_max=np.max(frame[...][...][0])
-    Cb_min=np.min(frame[...][...][2])
-    Cb_max=np.max(frame[...][...][2])
-    Cr_min=np.min(frame[...][...][1])
-    Cr_max=np.max(frame[...][...][1])
-    '''
+    Y_min=np.min(frame_ycbcr[...][...][0])
+    Y_max=np.max(frame_ycbcr[...][...][0])
+    Cb_min=np.min(frame_ycbcr[...][...][2])
+    Cb_max=np.max(frame_ycbcr[...][...][2])
+    Cr_min=np.min(frame_ycbcr[...][...][1])
+    Cr_max=np.max(frame_ycbcr[...][...][1])
+    #'''
 
-    '''
+
+    #'''
     #Rule Set - I
-    Y_mean=np.mean(frame[...][...][0])
-    Cb_mean=np.mean(frame[...][...][2])
-    Cr_mean=np.mean(frame[...][...][1])
+    Y_mean=np.mean(frame_ycbcr[...][...][0])
+    Cb_mean=np.mean(frame_ycbcr[...][...][2])
+    Cr_mean=np.mean(frame_ycbcr[...][...][1])
+    #'''
+    
 
     '''
     #Rule Set - II
-    '''
     def fu(val):
         return (-(2.6*pow(10,-10)*pow(val,7))
         +(3.3*pow(10,-7)*pow(val,6))
@@ -65,42 +67,41 @@ def Localize_Fire(frame):
         -(2.05*pow(10,3)*val)
         +(7.29*pow(10,4)))
     '''
+
     #iterate each pixel
     for x in range(frame_ycbcr.shape[1]):
         for y in range(frame_ycbcr.shape[0]):
             if frame_ycbcr[y][x][0]>frame_ycbcr[y][x][2] and frame_ycbcr[y][x][1]>frame_ycbcr[y][x][2]:
-                #if frame_ycbcr[y][x][0]>Y_mean and frame_ycbcr[y][x][2]<Cb_mean and frame_ycbcr[y][x][1]>Cr_mean:
+                if frame_ycbcr[y][x][0]>Y_mean and frame_ycbcr[y][x][2]<Cb_mean and frame_ycbcr[y][x][1]>Cr_mean:
+                    #if (frame_ycbcr[y][x][2]>=fu(frame_ycbcr[y][x][1])) and (frame_ycbcr[y][x][2]<=fd(frame_ycbcr[y][x][1])) and (frame_ycbcr[y][x][2]<=fl(frame_ycbcr[y][x][1])):
 
-            #if (frame[y][x][2]>=fu(frame[y][x][1])) and (frame[y][x][2]<=fd(frame[y][x][1])) and (frame[y][x][2]<=fl(frame[y][x][1])):
-            #if abs(frame_ycbcr[y][x][2]-frame_ycbcr[y][x][1])>200:
-                        #Rule Set - 0
-                    #norm_Y_Cb=2*(((frame[y][x][0]-frame[y][x][2])-(Y_min-Cb_max))/((Y_max-Cb_min)-(Y_min-Cb_max)))-1
-                    #norm_Cr_Cb=2*(((frame[y][x][1]-frame[y][x][2])-(Cr_min-Cb_max))/((Cr_max-Cb_min)-(Cr_min-Cb_max)))-1
+                    #Rule Set - 0
+                    norm_Y_Cb=2*(((frame_ycbcr[y][x][0]-frame_ycbcr[y][x][2])-(Y_min-Cb_max))/((Y_max-Cb_min)-(Y_min-Cb_max)))-1
+                    norm_Cr_Cb=2*(((frame_ycbcr[y][x][1]-frame_ycbcr[y][x][2])-(Cr_min-Cb_max))/((Cr_max-Cb_min)-(Cr_min-Cb_max)))-1
 
-                #fuzzy rule set
-                Y_Cb=frame[y][x][0]-frame[y][x][2]
-                Cr_Cb=frame[y][x][1]-frame[y][x][2]
+                    Y_Cb=frame_ycbcr[y][x][0]-frame_ycbcr[y][x][2]
+                    Cr_Cb=frame_ycbcr[y][x][1]-frame_ycbcr[y][x][2]
 
-                norm_Y_Cb=(2*((Y_Cb-Y_Cb_min)/(Y_Cb_max-Y_Cb_min)))-1
-                norm_Cr_Cb=(2*((Cr_Cb-Cr_Cb_min)/(Cr_Cb_max-Cr_Cb_min)))-1
+                    norm_Y_Cb=(2*((Y_Cb-Y_Cb_min)/(Y_Cb_max-Y_Cb_min)))-1
+                    norm_Cr_Cb=(2*((Cr_Cb-Cr_Cb_min)/(Cr_Cb_max-Cr_Cb_min)))-1
 
-                if(abs(norm_Y_Cb)>=0 and abs(norm_Y_Cb)<=0.5) and (abs(norm_Cr_Cb)>=0.7 and abs(norm_Cr_Cb)<=1):
-                    mask[y][x][0]=225
-                    mask[y][x][1]=225
-                    mask[y][x][2]=225
-                    pixel+=1
-                '''
-                if norm_Y_Cb>=-1 and norm_Y_Cb<=1 and norm_Cr_Cb>=-1 and norm_Cr_Cb<=1:
-                    #print("norm_Y_Cb : "+str(norm_Y_Cb))
-                    #print("norm_Cr_Cb : "+str(norm_Cr_Cb))
-                    continue
-                else:
-                    #print("norm_Cr_Cb : "+str(norm_Cr_Cb))
-                    #print("norm_Y_Cb : "+str(norm_Y_Cb))
-                    #print("Error.. Overflow !")
-                    norm_error+=1
-                    #sleep(1000)
-                '''
+                    if norm_Y_Cb>=-1 and norm_Y_Cb<=1 and norm_Cr_Cb>=-1 and norm_Cr_Cb<=1:
+                        #print("norm_Y_Cb : "+str(norm_Y_Cb))
+                        #print("norm_Cr_Cb : "+str(norm_Cr_Cb))
+                        if(abs(norm_Y_Cb)>=0.5 and abs(norm_Y_Cb)<=1) and (abs(norm_Cr_Cb)>=0.0 and abs(norm_Cr_Cb)<=1):
+                        #if(abs(norm_Y_Cb)>=0 and abs(norm_Y_Cb)<=1):
+                            mask[y][x][0]=225
+                            mask[y][x][1]=225
+                            mask[y][x][2]=225
+                            pixel+=1
+                        #'''
+                    else:
+                        #print("norm_Cr_Cb : "+str(norm_Cr_Cb))
+                        #print("norm_Y_Cb : "+str(norm_Y_Cb))
+                        #print("Error.. Overflow !")
+                        norm_error+=1
+                        #sleep(1000)
+                    #'''
 
     #mask=cv.cvtColor(mask,cv.COLOR_YCR_CB2RGB)
     
@@ -196,7 +197,7 @@ if __name__=="__main__":
             print("Frames Processed : "+str(count))
             print("FPS : "+str(round(1/(stop-start))))
             print("Pixels Detected : "+str(pixel))
-            #print("Normalized Value Overflow : "+str(norm_error))
+            print("Normalized Value Overflow : "+str(norm_error))
  
             #waiting for quit keypress
             if cv.waitKey(1) & 0XFF == ord('q'):